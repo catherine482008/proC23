7f820b395d7ee2dcdaee802d0bfcf59e4886b651
catherine482008 <72607852+catherine482008@users.noreply.github.com>
2020-12-04 19:08:57 +0530

    Add files via upload

---
+++ redZone.js
@@ -0,0 +1,14 @@
+class redZone{
+    constructor(x,y,width,height){
+        this.body=Bodies.rectangle(x,y,width,height)
+        World.add(world,this.body)
+        this.width=width
+        this.height=height
+    }
+    display(){
+        var pos=this.body.position;
+        fill("red")
+        rect(pos.x,pos.y,this.width,this.height)
+        
+    }
+}
